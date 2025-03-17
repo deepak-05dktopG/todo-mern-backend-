543b1afe0571958303de38ccbdaec3cdc23f2cee
deepak-05dktopG <kumardeepak59422@gmail.com>
2025-03-17 21:45:52 +0530

    backend completed (CRUD)

--- server.js
+++ server.js
@@ -1,64 +1,89 @@
 //Express
-const express = require('express');
-const app=express();
-app.use(express.json())
+const express = require("express");
+const app = express();
+app.use(express.json());
 const PORT = 3000;
-const mongoose=require('mongoose');
-
+const mongoose = require("mongoose");
 
 //connecting mongodb
-mongoose.connect('mongodb://localhost:27017/mern-app')
-.then(()=>{
+mongoose
+  .connect("mongodb://localhost:27017/mern-app")
+  .then(() => {
     console.log("connected to database");
-})
-.catch((err)=>{
-    console.log("Error in DB is : ",err);
-})
+  })
+  .catch((err) => {
+    console.log("Error in DB is : ", err);
+  });
 
 //creating Schema
-const todoSchema=new mongoose.Schema({
-    title:String,
-    description:String,
-})
+const todoSchema = new mongoose.Schema({
+  title: { required: true, type: String },
+  description: String,
+});
 //creating model
-const todoModel= mongoose.model('Todo',todoSchema)
+const todoModel = mongoose.model("Todo", todoSchema);
 
 //create a new todo item
-app.post('/todos',async(req,res)=>{
-    const {title,description} = req.body;
+app.post("/todos", async (req, res) => {
+  const { title, description } = req.body;
 
-try{
-    const newTodo = new todoModel({title,description});
-    await newTodo.save()
+  try {
+    const newTodo = new todoModel({ title, description });
+    await newTodo.save();
     res.status(201).json(newTodo);
-}
-catch(err){
-        console.log(err); 
-        res.status(500).json({message:"Error in creating new todo item"})
-    }
-})
-
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error in creating new todo item" });
+  }
+});
 
 //get all items
-app.get("/todos",(req,res)=>{
-    res.json('mongodb://localhost:27017/mern-app')
-})
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await todoModel.find();
+    res.json(todos);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error in creating new todo item" });
+  }
+  res.json("mongodb://localhost:27017/mern-app");
+});
 
+//update all items
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { title, description } = req.body;
+    const id = req.params.id;
 
+    const updatedTodo = await todoModel.findByIdAndUpdate(
+      id,
+      { title, description },
+      { new: true }
+    );
 
+    if (!updatedTodo) {
+      return res.status(404).json({ message: "Todo not found" });
+    }
+    res.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error in creating new todo item" });
+  }
+});
 
-
-
-
-
-
-
-
-
-
-
+//delete todos
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    await todoModel.findByIdAndDelete(id);
+    res.status(204).end();
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error in creating new todo item" });
+  }
+}); 
 
 //Start the server
-app.listen(PORT,()=>{
-    console.log("Server is running on PORT: ",PORT);
-})
+app.listen(PORT, () => {
+  console.log("Server is running on PORT: ", PORT);
+});
